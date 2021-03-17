a5475c35eb1f9afc442514d5b7b5cb6facc29ecf
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2021-03-17 09:12:49 -0400

    Update scripts.js

---
+++ scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $("a[href^='http://']").each(
+        function() {
+            if (this.href.indexOf(location.hostname) == -1) {
+                $(this).attr('target', '_blank');
+            }
+        }
+    );
+    $("a[href^='https://']").each(
+        function() {
+            if (this.href.indexOf(location.hostname) == -1) {
+                $(this).attr('target', '_blank');
+            }
+        }
+    );
+});
